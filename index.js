fad8cc330f06b421d54690e82d2d1f323ccb203f
training@hashiba.org <training@hashiba.org>
2017-11-11 23:31:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ let newID = 1000000000 + oldID;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.floor(Math.random() * 20);
 
-let randomInteger = Number.isInteger(randomNumber);
+let randomInteger = Math.floor(randomNumber);
+
